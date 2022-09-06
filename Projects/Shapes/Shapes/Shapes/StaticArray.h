9aed7394a06f3e38a76289fc3a2fcd79d20f9d1b
Santiago Rafael Campo Cotes <44332093+GaxysGalactic@users.noreply.github.com>
2022-09-06 11:52:10 -0500

    Part 4 - StaticArray Header
    + Added header for TStaticArray

--- Projects/Shapes/Shapes/Shapes/StaticArray.h
+++ Projects/Shapes/Shapes/Shapes/StaticArray.h
@@ -12,4 +12,16 @@ public:
 	T operator[](const int Index);
 
 	T operator[](const int Index) const;
+
+	T front();
+
+	T front() const;
+
+	T back();
+
+	T back() const;
+
+	T* GetData();
+
+	T* GetData() const;
 };
