946582f38980b26325337280156ac3c04d3321e9
Santiago Rafael Campo Cotes <44332093+GaxysGalactic@users.noreply.github.com>
2022-09-12 16:00:45 -0500

    Part 2 - Access
    + Added methods to allow for access to elements in the array

--- Projects/LinkedListIterator/LinkedListIterator/LinkedListIterator/SLList.h
+++ Projects/LinkedListIterator/LinkedListIterator/LinkedListIterator/SLList.h
@@ -28,10 +28,50 @@ public:
 
 	FSLList(const T& FirstElement)
 	{
-		Head = new FNode()->Element = FirstElement;
+		Head = new FNode();
+		Head -> Element = FirstElement;
 		Tail = Head;
 		Size = 1;
 	}
 
+	T& operator[](const int Index)
+	{
+		FNode* Current = Head;
+		for (int i = 0; i < Index; i++)
+		{
+			Current = Current->Next;
+		}
+		return Current->Element;
+	}
+
+	const T& operator[](const int Index) const
+	{
+		FNode* Current = Head;
+		for (int i = 0; i < Index; i++)
+		{
+			Current = Current->Next;
+		}
+		return Current->Element;
+	}
+
+	T& Head()
+	{
+		return Head->Element;
+	}
+
+	const T& Head() const
+	{
+		return Head->Element;
+	}
+
+	T& Tail()
+	{
+		return Tail->Element;
+	}
+
+	const T& Tail() const
+	{
+		return Tail->Element;
+	}
 
 };
