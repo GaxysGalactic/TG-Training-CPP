461a1c6f1ab225bdaed8a2ca481edd7fd806a891
Santiago Rafael Campo Cotes <44332093+GaxysGalactic@users.noreply.github.com>
2022-09-28 15:08:02 -0500

    FlappyBird Progress Day 1
    It kinda works. Collision detection needs to be better and better to rename stuff and store stuff better

---
+++ Projects/PixelGameEngine/PixelTutorials/FlappyBird/main.cpp
@@ -0,0 +1,203 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+// Override base class with your custom functionality
+class FFlappyBird : public olc::PixelGameEngine
+{
+public:
+	FFlappyBird()
+	{
+		// Name your application
+		sAppName = "Flappy Bird";
+	}
+
+	class FBird
+	{
+	
+	public:
+
+		struct FCoord
+		{
+			float X = 0.0f;
+			float Y = 0.0f;
+		};
+
+
+	};
+
+private:
+	float BirdPosition = 0.0f;
+	float BirdVelocity = 0.0f;
+	float BirdAcceleration = 0.0f;
+
+	float Gravity = 800.0f;
+
+	float SectionWidth;
+	std::list<int> ListSection;
+	float LevelPosition = 0.0f;
+
+	bool bHasCollided = false;
+
+	int AttemptCount = 0;
+	int FlapCount = 0;
+	int MaxFlapCount = 0;
+
+	bool bResetGame = true;
+
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		ListSection = { 0, 0, 0, 0 };
+		SectionWidth = (float)ScreenWidth() / (float)(ListSection.size() - 1);
+
+		return true;
+	}
+
+	bool OnUserUpdate(float ElapsedTime) override
+	{
+		if (bResetGame)
+		{
+			bHasCollided = false;
+			bResetGame = false;
+			ListSection = { 0, 0, 0, 0 };
+			BirdAcceleration = 0.0f;
+			BirdVelocity = 0.0f;
+			BirdPosition = ScreenHeight() / 2.0f;
+			FlapCount = 0;
+			++AttemptCount;
+		}
+
+
+		if (bHasCollided)
+		{
+			//Do nothing until user releases space
+			if (GetKey(olc::Key::SPACE).bReleased)
+			{
+				bResetGame = true;
+			}
+		}
+		else
+		{
+
+			if (GetKey(olc::Key::SPACE).bPressed)
+			{
+				BirdAcceleration = 0.0f;
+				BirdVelocity = -Gravity / 6.0f;
+				++FlapCount;
+				if (FlapCount > MaxFlapCount)
+				{
+					MaxFlapCount = FlapCount;
+				}
+			}
+			else
+			{
+				BirdAcceleration += Gravity * ElapsedTime;
+			}
+
+			if (BirdAcceleration >= Gravity)
+			{
+				BirdAcceleration = Gravity;
+			}
+
+
+			BirdVelocity += BirdAcceleration * ElapsedTime;
+			BirdPosition += BirdVelocity * ElapsedTime;
+
+			int BirdX = (int)(ScreenWidth() / 3.0f);
+
+			LevelPosition += 14.0 * 8 * ElapsedTime;
+
+			if (LevelPosition > SectionWidth)
+			{
+				LevelPosition -= SectionWidth;
+				ListSection.pop_front();
+				int i = std::rand() % (int)(ScreenHeight() * 0.8);
+				if (0.85f * ScreenHeight() <= i && i <= 0.15f * ScreenHeight())
+				{
+					i = 0;
+				}
+				ListSection.push_back(i);
+			}
+
+			FillRect(0, 0, ScreenWidth(), ScreenHeight(), olc::BLACK);
+
+			int Section = 0;
+			for (int s : ListSection)
+			{
+				if (s != 0)
+				{
+					int SectionStart = Section * SectionWidth;
+					int Offset = 0.25f * SectionWidth;
+					int GapSize = 0.4f * SectionWidth;
+
+					int RectangleWidth = SectionWidth / 4;
+					int RectangleStartX = SectionStart + Offset - LevelPosition;
+					int RectangleEndX = RectangleStartX + RectangleWidth;
+
+					int LowerRectangleStartY = ScreenHeight() - s;
+					int UpperRectangleHeight = ScreenHeight() - s - GapSize;
+
+					FillRect(
+						RectangleStartX,
+						LowerRectangleStartY,
+						RectangleWidth,
+						ScreenHeight(),
+						olc::GREEN);
+					FillRect(
+						RectangleStartX,
+						0,
+						RectangleWidth,
+						UpperRectangleHeight,
+						olc::GREEN);
+
+					if (Section == 1)
+					{
+						bHasCollided =
+							BirdPosition < 2
+							|| BirdPosition > ScreenHeight() - 2
+							|| (BirdPosition > LowerRectangleStartY && BirdX > RectangleStartX && BirdX < RectangleEndX)
+							|| (BirdPosition < UpperRectangleHeight && BirdX > RectangleStartX && BirdX < RectangleEndX);
+					}
+
+				}
+				Section++;
+			}
+
+			/*Collision Detection
+			bHasCollided =
+				BirdPosition < 2
+				|| BirdPosition > ScreenHeight() - 2
+				|| m_bufScreen[(int)(BirdPosition + 0) * ScreenWidth() + BirdX].Char.UnicodeChar != L' '
+				|| m_bufScreen[(int)(BirdPosition + 8) * ScreenWidth() + BirdX].Char.UnicodeChar != L' '
+				|| m_bufScreen[(int)(BirdPosition + 0) * ScreenWidth() + BirdX + 48].Char.UnicodeChar != L' '
+				|| m_bufScreen[(int)(BirdPosition + 8) * ScreenWidth() + BirdX + 48].Char.UnicodeChar != L' ';*/
+
+
+			// Draw Bird
+			if (BirdVelocity > 0)
+			{
+				DrawString(BirdX, BirdPosition, "\\\\\\");
+				DrawString(BirdX, BirdPosition + 8, "<\\\\\\=Q");
+			}
+			else
+			{
+				DrawString(BirdX, BirdPosition, "<///=Q");
+				DrawString(BirdX, BirdPosition + 8, "///");
+			}
+
+			DrawString(1, 1, "Attempt: " + std::to_string(AttemptCount) + " Score: " + std::to_string(FlapCount) + " High Score: " + std::to_string(MaxFlapCount));
+			
+		}
+
+		return true;
+	}
+};
+
+int main()
+{
+	FFlappyBird Game;
+	if (Game.Construct(720, 350, 2, 2))
+		Game.Start();
+	return 0;
+}
